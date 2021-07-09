8f5ac42859fded14ab2fc9b8eb9ca0a68ca717c2
Jorge Alves <jorgeedualves84@gmail.com>
2021-07-09 14:29:32 -0300

    print char, int and decimal

--- ft_printf.c
+++ ft_printf.c
@@ -29,13 +29,45 @@
 //         character = handle_types(opcao.type);
 // }
 
-static void types_identifier(const char *format, int *i, int *len)
+static void types_identifier(const char *format, int *i, int *len, va_list args)
 {
+   char *print_c;
+   //char *print_s;
+
     t_flags fl;
     if (format[*i] != '%')
     {
-       if (ft_strchr_01(CONVERSIONS, format[*i]))
-            fl.type = format[(*i)++]; // 'A'
+       if (ft_strchr_01(CONVERSIONS, format[*i])) // Se a ocorrencia de format[*i] estiver em CONVERSIONS
+       {
+            // printar char com a funcao ft_putchar:
+            fl.type = format[(*i)++]; // 'A' // "%c"
+            if (fl.type == 'c')
+                print_c = va_arg(args, char *);
+            ft_putchar(*print_c);
+            // printar string com a funcao 
+      /*    if (fl.type == 's')
+                print_s = va_arg(args, char *);
+            ft_strchr(print_s, len);
+            if (fl.type == 'p')
+               print_c  = va_arg(args, char *);
+            write(1, &print_c, 1);
+            // printar int e decimal com a funcao ft_putnbr
+            if (fl.type == 'd' || fl.type == 'i')
+               ft_putnbr(va_arg(args, int));
+            if (fl.type == 'u')
+               print_c  = va_arg(args, char *);
+            write(1, &print_c, 1);
+            if (fl.type == 'x')
+               print_c  = va_arg(args, char *);
+            write(1, &print_c, 1);
+            if (fl.type == 'X')
+               print_c  = va_arg(args, char *);
+            write(1, &print_c, 1);
+            if (fl.type == '%')
+               print_c  = va_arg(args, char *);
+            write(1, &print_c, 1);
+            */
+       }
     }
     else
 		(*len) = -1;
@@ -57,7 +89,7 @@ int ft_printf(const char *format, ...)
         else
         {
             i++;
-            types_identifier(format, &i, &len);
+            types_identifier(format, &i, &len, args);
             if (len == -1)
 				return (-1);  // %[flags][width][.precision][size]type -> "hello" "    hello"
         }
@@ -68,10 +100,20 @@ int ft_printf(const char *format, ...)
 
 int main (void)
 {
+   
+   //integer
+   int number = 123456789;
+   ft_printf("%i", number);
+   
 
-    ft_printf("Este é o caractere %c", 'A');
+   /*
+   //string
+    ft_printf("Hello %s", "Jorge e Alexandre e Alfredo");
+    ft_printf("\n");
+   */
     return (0);
 }
+
 // 1 coisa: tratar e printar o char
 // ao encontrar a flag, tratar e printar 
 // começar com o char, depois string
--- ft_printf.h
+++ ft_printf.h
@@ -13,16 +13,19 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <stdarg.h>
-# include <stdarg.h>
 # include <unistd.h>
 # include <stdio.h>
+# include "stdlib.h"
+# include "string.h"
+
 
 # define CONVERSIONS	"cspdiuxX%"
 # define ALL_FLAGS "-0.*0123456789cspdiuxX%"
-# define NUMBERS "123456789"
+# define NUMBERS		"0123456789"
 
 typedef struct {
     char type;
+    int  integer;
     int width;
     int precision;
 } t_flags;
@@ -30,6 +33,17 @@ typedef struct {
 int ft_printf(const char *format, ...);
 void	ft_putchar_len(char c, int *len);
 int		ft_strchr_01(char *s, char c);
-void		print_spec_c(int *len, t_flags fl, char c);
+void		print_c(char c, int *len);
+
+//para imprimir integer:
+size_t	ft_strlen(const char *s);
+void	ft_putstr(char *s);
+void	ft_putchar(char c);
+void	ft_putnbr(int n);
+char	*ft_itoa(int n);
+void	ft_putstr_len(char *s, int *len);
+void	print_s(char *c, int *len);
+void    print_int(t_flags fl, va_list args);
+
 
 #endif
