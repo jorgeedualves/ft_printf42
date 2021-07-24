46fcfdb9c1864edd39b21b07141ab47a575576e3
Jorge Alves <jorgeedualves84@gmail.com>
2021-07-24 11:10:52 -0300

    modified: ft_printf_utils.c

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -12,107 +12,43 @@
 
 #include "ft_printf.h"
 
-void		print_c(char c, int *len, t_flags fl)
+void	ft_putchar_len(char c, int *len)
 {
-	if (fl.minus == 0 && fl.width == 0)
-	{
-		ft_putchar_len(c, len);	
-	}
-	else if (fl.minus == 0 && fl. width > 0)
-	{
-		while (fl.width-- > 1)
-		{
-			write(1, " ", 1);
-			len++;
-		}
-		ft_putchar_len(c, len);
-	}
-	else if (fl.minus == 1)
-	{
-		ft_putchar_len(c, len);
-		while (fl.width-- > 1)
-		{
-			write(1, " ", 1);
-			len++;
-		}
-	}
+	write(1, &c, 1);
+	(*len)++;
 }
-void	print_s(char *c, int *len, t_flags fl)
-{
-	int size;
 
-	size = (int)ft_strlen(c);
-	if ((fl.precision == 0 && fl.width == 0) || (fl.precision >= size && fl.width <= size))
-		ft_putstr_len(c, len); 
-	else if (fl.precision < size && (fl.width == 0 || fl.width < size))
-		while (fl.precision > 0)
-		{
-			write(1, &*c, 1);
-			fl.precision--, len++, c++;				
-		}
-	else if ((fl.precision == 0 || fl.precision >= size) && fl.width > size ) //imprime espaços + string
-		print_s_space_print(c, len, fl);
-	else if (fl.precision < size && fl.width >= size) //imprime espaços + corta string
-		print_s_space_cut(c, len, fl);
-}
-void	print_s_space_print(char *c, int *len, t_flags fl)
+int	ft_strchr(char *s, char c)
 {
-	int size;
+	int	i;
 
-	size = (int)ft_strlen(c);
-	if (fl.minus == 0)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		while (fl.width > size)
-		{
-			write(1, " ", 1);
-			fl.width--, len++;
-		}
-		ft_putstr_len(c, len);
+		if (s[i++] == c)
+			return (1);
 	}
-	else
-	{
-		ft_putstr_len(c, len);
-		while (fl.width > size)
-		{
-			write(1, " ", 1);
-			fl.width--, len++;
-		}
-	}
+	return (0);
 }
-void	print_s_space_cut(char *c, int *len, t_flags fl)
-{
-	int size;
 
-	size = (int)ft_strlen(c);
-	if(fl.minus == 0)
-	{
-		while (fl.width - fl.precision > 0)
-		{
-			write(1, " ", 1);
-			fl.width--, len++;
-		}
-		while (fl.precision > 0)
-		{
-			write(1, &*c, 1);
-			fl.precision--, len++, c++;				
-		}
-	}
-	else
-	{
-		while (fl.precision > 0)
-		{
-			write(1, &*c, 1);
-			fl.precision--, len++, c++;				
-		}
-		while (fl.width - fl.precision > 0)
-		{
-			write(1, " ", 1);
-			fl.width--, len++;
-		}
-	}
-}
-void	print_i_d(t_flags fl, va_list args, int *len)
+size_t	ft_strlen(const char *s)
 {
-	fl.strNum = ft_itoa(va_arg(args, int));
-	ft_putstr_len(fl.strNum, len);
+	size_t	i;
+
+	i = 0;
+	while (*(s++))
+		i++;
+	return (i);
+}
+
+void	ft_putstr(char *s)
+{
+	if (!s)
+		return ;
+	write(1, s, ft_strlen(s));
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
