55ef84bf088ab40cb9ced7fb05646705fef97aa8
Jorge Alves <jorgeedualves84@gmail.com>
2021-07-24 11:12:26 -0300

    modified: ft_printf_utils2.c

--- ft_printf_utils2.c
+++ ft_printf_utils2.c
@@ -12,32 +12,71 @@
 
 #include "ft_printf.h"
 
-void 	print_u(t_flags fl, va_list args, int *len)
+void	ft_putnbr(int n)
 {
-	fl.strNum = ft_uitoa(va_arg(args, unsigned int));
-	ft_putstr_len(fl.strNum, len);	
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	if (n < 0 && n != -2147483648)
+	{
+		ft_putchar('-');
+		n = n * -1;
+	}
+	if (n <= 9 && n >= 0)
+		ft_putchar(n + 48);
+	else if (n > 0)
+	{
+		ft_putnbr(n / 10);
+		ft_putchar(n % 10 + 48);
+	}
 }
 
-void	print_p(t_flags fl, va_list args, int *len)
+int	ft_len(int num)
 {
-	fl.strNum = ft_int_to_hex_pxX(va_arg(args, unsigned long int), fl);
-	ft_putstr_len_p(fl.strNum, len);
+	size_t	len;
+
+	if (num <= 0)
+		len = 1;
+	else
+		len = 0;
+	while (num != 0)
+	{
+		num = num / 10;
+		len++;
+	}
+	return (len);
 }
 
-void	print_x(t_flags fl, va_list args, int *len)
+int	ft_to_positive(int num)
 {
-	fl.strNum = ft_int_to_hex_pxX(va_arg(args, unsigned long int), fl);
-	ft_putstr_len(fl.strNum, len);
+	if (num < 0)
+		return (-num);
+	else
+		return (num);
 }
 
-void	print_X(t_flags fl, va_list args, int *len)
+char	*ft_itoa(int n)
 {
-	fl.strNum = ft_int_to_hex_pxX(va_arg(args, unsigned long int), fl);
-	ft_putstr_len(fl.strNum, len);
-}
+	int		sign;
+	int		len;
+	char	*result;
 
-void print_pct(int *len)
-{
-	ft_putstr_len("%", len);
+	if (n < 0)
+		sign = -1;
+	else
+		sign = 1;
+	len = ft_len(n);
+	result = (char *)malloc(sizeof(char) * len + 1);
+	if (result == NULL)
+		return (0);
+	result[len] = '\0';
+	len--;
+	while (len >= 0)
+	{
+		result[len] = '0' + ft_to_positive(n % 10);
+		n = ft_to_positive(n / 10);
+		len--;
+	}
+	if (sign == -1)
+		result[0] = '-';
+	return (result);
 }
-
