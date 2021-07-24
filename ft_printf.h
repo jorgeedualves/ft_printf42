7ed30a94021f8d992e6411dac80f23c3f76ca856
Jorge Alves <jorgeedualves84@gmail.com>
2021-07-24 10:53:17 -0300

    Atualizacao: ft_print.h

--- ft_printf.h
+++ ft_printf.h
@@ -19,9 +19,7 @@
 # include "string.h"
 # define CONVERSIONS	"cspdiuxX%"
 # define FLAGS			"-0.0123456789"
-# define ALL_FLAGS		"-0.0123456789cspdiuxX%"
 # define NUMBERS		"0123456789"
-
 
 typedef struct t_flags
 {
@@ -34,36 +32,56 @@ typedef struct t_flags
 	char	*strNum;
 }	t_flags;
 
-void		print_c(char c, int *len, t_flags fl);
+// utils
+void	ft_putchar_len(char c, int *len);
+int		ft_strchr(char *s, char c);
+size_t	ft_strlen(const char *s);
+void	ft_putstr(char *s);
+void	ft_putchar(char c);
+
+//utils_2
+void	ft_putnbr(int n);
+int		ft_len(int num);
+int		ft_to_positive(int num);
+char	*ft_itoa(int n);
+
+//utils_3
+t_flags	ft_clean_flags(void);
+void	ft_putstr_len(char *s, int *len);
+void	print_pct(int *len);
+
+//print_c
+void	print_c(char c, int *len, t_flags fl);
+
+//print_s
 void	print_s(char *c, int *len, t_flags fl);
 void	print_s_space_print(char *c, int *len, t_flags fl);
 void	print_s_space_cut(char *c, int *len, t_flags fl);
+void	print_s_space_cut_right(char *c, int *len, t_flags fl);
+void	print_s_space_cut_left(char *c, int *len, t_flags fl);
+
+//print_id
 void	print_i_d(t_flags fl, va_list args, int *len);
+void	print_space(t_flags fl, int size, int *len);
+void	print_i_d_zero(t_flags fl, int size, int *len);
 
-void 	print_u(t_flags fl, va_list args, int *len);
-void	print_p(t_flags fl, va_list args, int *len);
-void	print_x(t_flags fl, va_list args, int *len);
-void	print_X(t_flags fl, va_list args, int *len);
-void 	print_pct(int *len);
-
-int	ft_len_hex(unsigned long int x);
-char	*ft_int_to_hex_pxX(unsigned long int n, t_flags fl);
-void	ft_putchar(char c);
-void	ft_putchar_len(char c, int *len);
-int	ft_strchr(char *s, char c);
-
-size_t	ft_strlen(const char *s);
-void	ft_putstr(char *s);
-void	ft_putstr_len(char *s, int *len);
-void	ft_putstr_len_p(char *s, int *len);
-void	ft_putnbr(int n);
-
-int	ft_len(int num);
-int	ft_ulen(unsigned int num);
-int	ft_to_positive(int num);
-char	*ft_itoa(int n);
+//print_u
+void	print_u(t_flags fl, va_list args, int *len);
+void	print_u_space_string(t_flags fl, int *len, int size);
+void	print_u_zero_string(t_flags fl, int *len, int size);
+int		ft_ulen(unsigned int num);
 char	*ft_uitoa(unsigned int n);
 
-t_flags	ft_clean_flags(void);
+//print_p
+void	print_p(t_flags fl, va_list args, int *len);
+void	print_p_no_zero(t_flags fl, int *len, int size);
+void	print_p_zero(t_flags fl, int *len, int size);
+void	ft_putstr_len_p(char *s, int *len);
+
+//print_xX
+void	print_xX(t_flags fl, va_list args, int *len);
+void	print_xX_right_aligned(t_flags fl, int *len, int size);
+int		ft_len_hex(unsigned long int x);
+char	*ft_int_to_hex_pxX(unsigned long int n, t_flags fl);
 
 #endif
