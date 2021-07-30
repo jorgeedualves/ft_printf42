909be925b10823e42fa18ebbaccd0be9b2cf2535
Jorge Alves <jorgeedualves84@gmail.com>
2021-07-30 18:37:19 -0300

    adjusting INT_MIN in the Print_id.c file

--- ft_printf.h
+++ ft_printf.h
@@ -22,6 +22,7 @@
 # define FLAGS			"-0.0123456789"
 # define NUMBERS		"0123456789"
 # define TYPES			"csidupxX%"
+
 # define HEXALOW    "0123456789abcdef"
 # define HEXAUPP    "0123456789ABCDEF" 
 
@@ -33,11 +34,30 @@ typedef struct t_flags
 	int		dot;
 	int		precision;
 	char	*strNum;
-	unsigned long long int	ulli;
-	long long int	lli;	
+
+	unsigned long long int    ulli;
+    long long int            lli;
+	
 }	t_flags;
 
 int	ft_printf(const char *format, ...);
+
+// utils
+void	ft_putchar_len(char c, int *len);
+int		ft_strchr(char *s, char c);
+size_t	ft_strlen(const char *s);
+void	ft_putstr(char *s);
+void	ft_putchar(char c);
+
+//utils_2
+void	ft_putnbr(int n);
+int		ft_len(int num);
+//int		ft_to_positive(int num);
+char	*ft_itoa(int n);
+
+//utils_3
+t_flags	ft_clean_flags(void);
+void	ft_putstr_len(char *s, int *len);
 
 //print_c
 void	print_c(char c, int *len, t_flags fl);
@@ -69,29 +89,17 @@ void	ft_putstr_len_p(char *s, int *len);
 char	*ft_int_to_hex_p(unsigned long int n, const char c);
 
 //print_xX
-void	print_xX(t_flags fl, va_list args, int *len, const char c);
+//void	print_xX(t_flags fl, va_list args, int *len, const char c);
+
+void    print_xX(t_flags fl, va_list args, int *len, const char c);
 void	print_xX_right_aligned(t_flags fl, int *len, int size);
 int		ft_len_hex(unsigned long int x);
 char	*ft_int_to_hex_pxX(unsigned long int n, const char c);
-char	*ft_ullitoa_base(unsigned long long int n, char *base);
+char    *ft_ullitoa_base(unsigned long long int n, char *base);
+
 int	return_hex_len(int num);
 
-// utils
-void	ft_putchar_len(char c, int *len);
-int		ft_strchr(char *s, char c);
-size_t	ft_strlen(const char *s);
-void	ft_putstr(char *s);
-void	ft_putchar(char c);
-
-//utils_2
-void	ft_putnbr(int n);
-int		ft_len(int num);
-int		ft_to_positive(int num);
-char	*ft_itoa(int n);
-
-//utils_3
-t_flags	ft_clean_flags(void);
-void	ft_putstr_len(char *s, int *len);
-void	print_pct(int *len);
+void	print_id_positive(t_flags fl, int number, int *len);
+void	print_id_negative(t_flags fl, int number, int *len);
 
 #endif
--- print_id.c
+++ print_id.c
@@ -17,7 +17,7 @@ void	print_i_d(t_flags fl, va_list args, int *len)
 	int number;
 
 	number = va_arg(args, int);
-	if (number > 0)
+	if (number >= 0)
 		print_id_positive(fl, number, len);
 	else	
 		print_id_negative(fl, number, len);
@@ -31,12 +31,12 @@ void	print_id_positive(t_flags fl, int number, int *len)
 	size = (int)ft_strlen(fl.strNum);
 	if (fl.width <= size && fl.precision <= size)
 		ft_putstr_len(fl.strNum, len);
-	if ( (fl.width > size && fl.precision == 0) || fl.precision > size)
+	if ((fl.width > size || fl.width == 0) && (fl.precision == 0 || fl.precision > size) && fl.zero == 1)
 	{
 		print_i_d_zero(fl, size, len);
 		ft_putstr_len(fl.strNum, len);
 	}
-	if ((fl.width > size && fl.precision != 0) && fl.precision <= size )
+	if ((fl.width > size && fl.precision > 0) && fl.precision <= size )
 	{	
 		if (fl.minus == 1)
 		{
@@ -52,16 +52,23 @@ void	print_id_positive(t_flags fl, int number, int *len)
 	free(fl.strNum);
 }
 
+
 void	print_id_negative(t_flags fl, int number, int *len)
 {
 	int size;
-
-	fl.strNum = ft_itoa(number);
-	size = (int)ft_strlen(fl.strNum);
+	if (number == -2147483648)
+		fl.strNum = "2147483648";
+	else
+		fl.strNum = ft_itoa(number);
+	size = (int)ft_strlen(fl.strNum) + 1; //pra compensar o sinal negativo que tiramos
 	if (fl.width <= size && fl.precision <= size)
-		ft_putstr_len(fl.strNum, len);
-	if ( (fl.width > size && fl.precision == 0) || fl.precision > size)
 	{
+		ft_putchar_len('-', len);
+		ft_putstr_len(fl.strNum, len);
+	}
+	if ((fl.width > size || fl.width == 0) && (fl.precision == 0 || fl.precision > size) && fl.zero == 1)
+	{
+		ft_putchar_len('-', len);
 		print_i_d_zero(fl, size, len);
 		ft_putstr_len(fl.strNum, len);
 	}
@@ -69,16 +76,19 @@ void	print_id_negative(t_flags fl, int number, int *len)
 	{	
 		if (fl.minus == 1)
 		{
+			ft_putchar_len('-', len);
 			ft_putstr_len(fl.strNum, len);
 			print_space(fl, size, len);
 		}
 		else
 		{
 			print_space(fl, size, len);
+			ft_putchar_len('-', len);
 			ft_putstr_len(fl.strNum, len);
 		}
 	}
-	free(fl.strNum);
+	if (number != -2147483648)
+		free(fl.strNum);
 }
 
 void	print_space(t_flags fl, int size, int *len)
@@ -102,6 +112,7 @@ void	print_i_d_zero(t_flags fl, int size, int *len)
 	}
 	else								// imprime zeros quando o width > zero
 	{
+		
 		while (fl.width - size > 0)
 		{
 			write(1, "0", 1);
